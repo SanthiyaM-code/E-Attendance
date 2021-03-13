f3197e06d47fc8d84f2d277a5918ec2e04ccb80f
SanthiyaM-code <santhiya10082001@gmail.com>
2021-03-13 21:20:59 +0530

    Save Class and Subject in Recyclerview

---
+++ app/src/main/java/com/codewithsandy/e_attendance/ClassItem.java
@@ -0,0 +1,29 @@
+package com.codewithsandy.e_attendance;
+
+public class ClassItem {
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    private  String className;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    private  String subjectName;
+
+    public ClassItem(String className,String subjectName)
+    {
+        this.className=className;
+        this.subjectName=subjectName;
+    }
+}
---
+++ app/src/main/java/com/codewithsandy/e_attendance/MainActivity.java
@@ -0,0 +1,58 @@
+package com.codewithsandy.e_attendance;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ClassAdapter classAdapter;
+    RecyclerView.LayoutManager layoutManager;
+
+    FloatingActionButton fab;
+
+    ArrayList<ClassItem> classItems=new ArrayList<>();
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab=findViewById(R.id.fab);
+        fab.setOnClickListener(v-> showDialog());
+
+        recyclerView=findViewById(R.id.recycleeview);
+        recyclerView.setHasFixedSize(true);
+        layoutManager =new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        classAdapter=new ClassAdapter(this,classItems);
+        recyclerView.setAdapter(classAdapter);
+
+    }
+    private void showDialog(){
+        MyDialog dialog=new MyDialog();
+        dialog.show(getSupportFragmentManager(),MyDialog.CLASS_ADD_DIALOG);
+        dialog.setListener((className,subjectName)->addClass(className,subjectName));
+    }
+
+    private void addClass(String className,String subjectName) {
+
+        classItems.add(new ClassItem(className,subjectName));
+        classAdapter.notifyDataSetChanged();
+    }
+
+
+}
